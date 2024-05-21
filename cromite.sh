19734136ebd2d2bc58419eae74613541c1b60ce1
Syhlx <syhlx@live.com>
2024-05-21 19:02:37 +0200

    Better handling of access to host policies

--- cromite.sh
+++ cromite.sh
@@ -1,17 +1,6 @@
 #!/usr/bin/bash
 
-# Merge the policies with the host ones.
-policy_root=/etc/chromium/policies
-
-for policy_type in managed recommended enrollment; do
-  policy_dir="$policy_root/$policy_type"
-  mkdir -p "$policy_dir"
-
-  if [[ -d "/run/host/$policy_root/$policy_type" ]]; then
-    find "/run/host/$policy_root/$policy_type" \
-      -maxdepth 1 -name '*.json' -type f \
-      -exec ln -sf '{}' "$policy_root/$policy_type" \;
-  fi
-done
+# Link the host's policies to sandbox
+[ -d "/etc/chromium" ] || ln -s /run/host/etc/chromium /etc/chromium
 
 exec cobalt "$@" --no-default-browser-check
